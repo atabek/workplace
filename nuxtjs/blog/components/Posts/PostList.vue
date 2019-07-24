62fdc9fa21c4cb76ee5bb6add77cff453f50ecdb
Excellent <excellent.nomad@gmail.com>
2019-07-24 22:59:20 +0600

    added admin layout

--- nuxtjs/blog/components/Posts/PostList.vue
+++ nuxtjs/blog/components/Posts/PostList.vue
@@ -2,18 +2,21 @@
   <section class="post-list">
     <PostPreview
         id="1"
+        :is-admin="isAdmin"
         thumbnail="https://www.brookings.edu/wp-content/uploads/2017/11/metro_20171121_tech-empowers-tech-polarizes-mark-muro.jpg"
         title="Hello there"
         previewText="This is my first post"
       />
       <PostPreview
         id="2"
+        :is-admin="isAdmin"
         thumbnail="https://www.brookings.edu/wp-content/uploads/2017/11/metro_20171121_tech-empowers-tech-polarizes-mark-muro.jpg"
         title="Hello there 2"
         previewText="This is my second post"
       />
       <PostPreview
         id="3"
+        :is-admin="isAdmin"
         thumbnail="https://www.brookings.edu/wp-content/uploads/2017/11/metro_20171121_tech-empowers-tech-polarizes-mark-muro.jpg"
         title="Hello there 3"
         previewText="This is my third post"
@@ -26,6 +29,12 @@ import PostPreview from "@/components/posts/PostPreview";
 export default {
   components: {
     PostPreview
+  },
+  props: {
+    isAdmin: {
+      type: Boolean,
+      default: false
+    }
   }
 }
 </script>
