b43c6ddacc2455319bfc957315db774b78c2975a
Excellent Nomad <excellent.nomad@gmail.com>
2019-08-02 16:31:20 +0600

    added unidirectional data flow

--- vuejs/src/components/User.vue
+++ vuejs/src/components/User.vue
@@ -2,10 +2,12 @@
     <div class="component">
         <h1>The User Component</h1>
         <p>I'm an awesome User!</p>
+        <button @click="changeName">Change name</button>
+        <p>Name: {{ name }}</p>
         <hr>
         <div class="row">
             <div class="col-xs-12 col-sm-6">
-                <app-user-detail></app-user-detail>
+                <app-user-detail :myName="name" @nameWasReset="name = $event"></app-user-detail>
             </div>
             <div class="col-xs-12 col-sm-6">
                 <app-user-edit></app-user-edit>
@@ -22,6 +24,18 @@
         components: {
             appUserDetail: UserDetail,
             appUserEdit: UserEdit
+        },
+        data: function() {
+            return {
+                name: 'Atabek',
+                // required: true,
+                default: 'Atabek'
+            }
+        },
+        methods: {
+            changeName() {
+                this.name = 'Akbalaev'
+            }
         }
     }
 </script>
--- vuejs/src/components/UserDetail.vue
+++ vuejs/src/components/UserDetail.vue
@@ -2,10 +2,27 @@
     <div class="component">
         <h3>You may view the User Details here</h3>
         <p>Many Details</p>
+        <p>User name: {{ switchName() }}</p>
+        <button @click="resetName">Reset name</button>
     </div>
 </template>
 
 <script>
+export default {
+    props: {
+        myName: {
+            type: String
+        }
+    },
+    methods: {
+        switchName() {
+            return this.myName.split('').reverse().join('')
+        },
+        resetName() {
+            this.$emit("nameWasReset", 'Ata')
+        }
+    },
+}
 </script>
 
 <style scoped>
