3c902bddd9de67edd2772246ba984d5bef972ba7
Excellent Nomad <excellent.nomad@gmail.com>
2019-08-06 10:22:04 +0600

    fill migration files

--- laravel/bigStore/database/migrations/2014_10_12_000000_create_users_table.php
+++ laravel/bigStore/database/migrations/2014_10_12_000000_create_users_table.php
@@ -6,29 +6,20 @@ use Illuminate\Database\Migrations\Migration;
 
 class CreateUsersTable extends Migration
 {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->string('name');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+            $table->boolean('is_admin')->default(false);
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
+            $table->softdeletes();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
     public function down()
     {
         Schema::dropIfExists('users');
--- laravel/bigStore/database/migrations/2019_08_06_040817_create_products_table.php
+++ laravel/bigStore/database/migrations/2019_08_06_040817_create_products_table.php
@@ -6,24 +6,20 @@ use Illuminate\Database\Migrations\Migration;
 
 class CreateProductsTable extends Migration
 {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
     public function up()
     {
         Schema::create('products', function (Blueprint $table) {
-            $table->bigIncrements('id');
+            $table->increments('id');
+            $table->string('name');
+            $table->string('description');
+            $table->unsignedInteger('units')->default(0);
+            $table->double('price');
+            $table->string('image');
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
     public function down()
     {
         Schema::dropIfExists('products');
