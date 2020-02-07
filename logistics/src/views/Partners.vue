b0b2479d11472f7d3039595224a9b3f380558330
Excellent <excellent.nomad@gmail.com>
2020-02-07 17:51:17 +0600

    added partners logo

--- logistics/src/views/Partners.vue
+++ logistics/src/views/Partners.vue
@@ -5,7 +5,7 @@
         <h2 class="heading-secondary">Наши партнеры</h2>
       </div>
       <div class="partners-logo">
-        <div class="logo-items" v-for="partner in partners" :key="partner.name">
+        <div class="logo-item" v-for="partner in partners" :key="partner.name">
           <img :src="partner.url" :alt="partner.alt" />
         </div>
       </div>
@@ -15,6 +15,19 @@
 <style lang="scss">
 .partners {
   width: 100%;
+}
+.partners-logo {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-evenly;
+}
+.logo-item {
+  max-width: 40%;
+  & img {
+    display: block;
+    width: 100%;
+    overflow: hidden;
+  }
 }
 </style>
 <script>
@@ -26,6 +39,51 @@ export default {
           name: "Mitsubishi Motors",
           url: require("../assets/partners/mmc.png"),
           alt: "Mitsubishi Motors logo"
+        },
+        {
+          name: "ISUZU MOTOR",
+          url: require("../assets/partners/isuzu.png"),
+          alt: "ISUZU MOTOR logo"
+        },
+        {
+          name: "Dai Nippon Construction",
+          url: require("../assets/partners/dnc.png"),
+          alt: "Dai Nippon Constrction logo"
+        },
+        {
+          name: "Fuji Electric",
+          url: require("../assets/partners/fujielectric.png"),
+          alt: "Fuji Electric logo"
+        },
+        {
+          name: "KOMATSU Tsusho",
+          url: require("../assets/partners/komatsu.png"),
+          alt: "KOMATSU Tsusho logo"
+        },
+        {
+          name: "Toyo Tires",
+          url: require("../assets/partners/toyotires.png"),
+          alt: "Toyo Tires logo"
+        },
+        {
+          name: "Mitsubishi Corp",
+          url: require("../assets/partners/mitsubishicorp.png"),
+          alt: "Sumitomo Corp logo"
+        },
+        {
+          name: "Sumitomo Corp",
+          url: require("../assets/partners/sumitomocorp.png"),
+          alt: "Sumitomo Corp logo"
+        },
+        {
+          name: "Showa Denkou",
+          url: require("../assets/partners/showadenkou.png"),
+          alt: "Toyota Tsusho logo"
+        },
+        {
+          name: "Toyota Tsusho",
+          url: require("../assets/partners/toyotatsusho.png"),
+          alt: "Toyota Tsusho logo"
         }
       ]
     };
