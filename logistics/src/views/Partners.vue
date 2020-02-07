3b949a9c39212288cd960151f618a72286c53f83
Excellent <excellent.nomad@gmail.com>
2020-02-07 17:10:14 +0600

    added a logo

--- logistics/src/views/Partners.vue
+++ logistics/src/views/Partners.vue
@@ -4,6 +4,31 @@
       <div class="row centered-text">
         <h2 class="heading-secondary">Наши партнеры</h2>
       </div>
+      <div class="partners-logo">
+        <div class="logo-items" v-for="partner in partners" :key="partner.name">
+          <img :src="partner.url" :alt="partner.alt" />
+        </div>
+      </div>
     </div>
   </div>
 </template>
+<style lang="scss">
+.partners {
+  width: 100%;
+}
+</style>
+<script>
+export default {
+  data: function() {
+    return {
+      partners: [
+        {
+          name: "Mitsubishi Motors",
+          url: require("../assets/partners/mmc.png"),
+          alt: "Mitsubishi Motors logo"
+        }
+      ]
+    };
+  }
+};
+</script>
